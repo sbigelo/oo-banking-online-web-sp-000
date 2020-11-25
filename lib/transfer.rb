d76b19fcd6eff6c24910ec5a4f30d57ee2896057
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-25 20:45:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,3 +13,19 @@ class Transfer
   end
   
 end
+
+
+ describe '#valid?' do
+    it "can check that both accounts are valid" do
+      expect(avi.valid?).to eq (true)
+      expect(amanda.valid?).to eq(true)
+      expect(transfer.valid?).to eq(true)
+    end
+
+    it "calls on the sender and receiver's #valid? methods" do
+      transfer_class = File.read("lib/transfer.rb")
+
+      expect(amanda).to receive(:valid?).and_return(true)
+      expect(avi).to receive(:valid?).and_return(true)
+
+      transfer.
