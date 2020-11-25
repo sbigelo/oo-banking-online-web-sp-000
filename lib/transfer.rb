8770d3c52eb8b2c3d228d21d24c39c38fd2cd5dd
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-25 20:45:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,17 +15,17 @@ class Transfer
 end
 
 
- describe '#valid?' do
-    it "can check that both accounts are valid" do
-      expect(avi.valid?).to eq (true)
-      expect(amanda.valid?).to eq(true)
-      expect(transfer.valid?).to eq(true)
-    end
+# describe '#valid?' do
+#     it "can check that both accounts are valid" do
+#       expect(avi.valid?).to eq (true)
+#       expect(amanda.valid?).to eq(true)
+#       expect(transfer.valid?).to eq(true)
+#     end
 
-    it "calls on the sender and receiver's #valid? methods" do
-      transfer_class = File.read("lib/transfer.rb")
+#     it "calls on the sender and receiver's #valid? methods" do
+#       transfer_class = File.read("lib/transfer.rb")
 
-      expect(amanda).to receive(:valid?).and_return(true)
-      expect(avi).to receive(:valid?).and_return(true)
+#       expect(amanda).to receive(:valid?).and_return(true)
+#       expect(avi).to receive(:valid?).and_return(true)
 
-      transfer.
+#       transfer.
