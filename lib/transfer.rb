76a595e78876b9e894bc022464c8cc9994106cf5
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-25 21:11:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if valid? && sender.balance > amount && self.status == "pending"
+    if valid? && sender.balance > amount && self.status == "complete"
       receiver.balance -= amount
       sender.balance += amount
       self.status = "reversed"
